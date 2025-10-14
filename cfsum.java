bd6f5b295c67790dba9d248656e17b3fed519c8a
Yogesh-017 <yogeshchavan1703@gmail.com>
2025-10-15 00:16:49 +0530

    Add files via upload

---
+++ cfsum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class cfsum {
+    public static void main(String[] args) {
+       Scanner sc= new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=1;i<=t;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+
+           if (a == b + c || b == a + c || c == a + b) {
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+       sc.close(); 
+    }
+}
