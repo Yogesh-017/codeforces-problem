35b96cee0826f46e720a3fb5535270fb2a0a0f40
Yogesh-017 <yogeshchavan1703@gmail.com>
2025-10-14 23:55:59 +0530

    Add files via upload

---
+++ codeforcesElephant.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class codeforcesElephant {
+    public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        
+        int ans = (x + 4) / 5;    
+        System.out.println(ans); 
+    }
+}
