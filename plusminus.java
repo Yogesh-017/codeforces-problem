381f7cebf0c83998b4ae9308af4b5045c9aa2e6b
yogesh murlidhar chavan <yogeshchavan1703@gmail.com>
2025-11-24 23:29:19 +0530

    Add files via upload

---
+++ plusminus.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class plusminus {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[3];
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < 3; j++) {
+                arr[j] = sc.nextInt();
+
+            }
+
+            if ((arr[0] + arr[1] == arr[2])) {
+                System.out.println("+");
+            } else {
+                System.out.println("-");
+            }
+        }
+
+        sc.close();
+
+    }
+}
