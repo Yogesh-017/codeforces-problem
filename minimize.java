efcaeff2ff1429ef74ac3eef331d0167e165654e
yogesh murlidhar chavan <yogeshchavan1703@gmail.com>
2025-12-30 09:56:10 +0530

    Add files via upload

---
+++ minimize.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class minimize {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int ans = Math.abs(a - b);
+            System.out.println(ans);
+
+        }
+        sc.close();
+
+    }
+
+}
