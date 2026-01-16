184cbb3c17e8c086df8b4773a0a003b806df9685
yogesh murlidhar chavan <yogeshchavan1703@gmail.com>
2026-01-16 23:50:28 +0530

    Add files via upload

---
+++ again.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class again {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        Long n = sc.nextLong();
+
+        System.out.println(25);
+
+        sc.close();
+    }
+}
