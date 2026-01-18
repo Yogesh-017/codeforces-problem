937be28bfc551931e64eff39ab924c26d8e587f2
yogesh murlidhar chavan <yogeshchavan1703@gmail.com>
2026-01-18 17:46:01 +0530

    Add files via upload

---
+++ socks.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class socks {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int count = 0;
+
+        while (n != 0) {
+
+            count++;
+            n--;
+            if (count % m == 0) {
+                n++;
+            }
+        }
+        System.out.println(count);
+        sc.close();
+
+    }
+
+}
